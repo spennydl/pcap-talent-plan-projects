1a312b2f398b62ef1a525710e83c69af312564a7
Spencer Leslie <spencerdleslie@gmail.com>
2019-06-16 22:58:54 -0700

    added documentation

--- src/lib.rs
+++ src/lib.rs
@@ -1,25 +1,78 @@
+#![deny(missing_docs)]
+//!
+//! Provides facilities for creating a managing an in-memory key value store
+//! 
+//! ```rust
+//! use kvs::KvStore;
+//! 
+//! fn main() {
+//!     // create a new store
+//!     let mut kvs = KvStore::new();
+//! 
+//!     // add some items
+//!     kvs.set("here comes".to_string(), "another one".to_string());
+//!     kvs.set("here it comes".to_string(), "again".to_string());
+//! 
+//!     // get some items
+//!     if let Some(val) = kvs.get("here comes".to_string()) {
+//!         println!("{} another one", val);
+//!     }
+//! 
+//!     // remove something
+//!     kvs.remove("here it comes".to_string());
+//! 
+//! }
+//! 
+
 use std::collections::HashMap;
 
+/// An in-memory key-value store.  Currently nothing more
+/// than a wrapper around a hash map.
 pub struct KvStore {
     store: HashMap<String, String>
 }
 
 
 impl KvStore {
+    /// Get a new KvStore.
+    /// 
+    /// ```rust
+    /// # use kvs::KvStore;
+    /// let mut kvs = KvStore::new();
+    /// ```
     pub fn new() -> KvStore {
         KvStore {
             store: HashMap::new()
         }
     }
 
+    /// Set an item in a store.
+    /// ```rust
+    /// # use kvs::KvStore;
+    /// # let mut kvs = KvStore::new();
+    /// kvs.set("my_key".to_string(), "my_value".to_string());
+    /// ```
     pub fn set(&mut self, key: String, val: String) {
         self.store.insert(key, val);
     }
 
+    /// Get a _copy_ of a value from the store.
+    /// ```rust
+    /// # use kvs::KvStore;
+    /// # let mut kvs = KvStore::new();
+    /// if let Some(value) = kvs.get("key".to_string()) {
+    ///     // process value
+    /// }
     pub fn get(&self, key: String) -> Option<String> {
         self.store.get(&key).cloned()
     }
 
+    /// Remove an item from the store.
+    /// ```rust
+    /// # use kvs::KvStore;
+    /// # let mut kvs = KvStore::new();
+    /// kvs.remove("key".to_string());
+    /// ```
     pub fn remove(&mut self, key: String) {
         self.store.remove(&key);
     }
